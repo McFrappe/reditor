5a706d47c3f20d3eeed7741ee4b3e3431d690a2d
McFrappe <obeyfranso@gmail.com>
2023-05-30 13:09:36 +0200

    feat: added status messages, if needed

--- src/editor.rs
+++ src/editor.rs
@@ -5,7 +5,8 @@ use termion::color;
 use termion::event::Key;
 
 use std::env;
-// use std::fmt::format;
+use std::time::Duration;
+use std::time::Instant;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 const STATUS_BG_COLOR: color::Rgb = color::Rgb(198, 156, 197);
@@ -16,12 +17,27 @@ const STATUS_FG_COLOR: color::Rgb = color::Rgb(63, 63, 63);
     pub y: usize,
 }
 
+struct StatusMessage {
+    text: String,
+    time: Instant,
+}
+
+impl StatusMessage {
+    fn from(message: String) -> Self {
+        Self {
+            time: Instant::now(),
+            text: message,
+        }
+    }
+}
+
 pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
     offset: Position,
     document: Document,
+    status_message: StatusMessage,
 }
 
 impl Editor {
@@ -41,9 +57,17 @@ impl Editor {
 
     pub fn default() -> Self {
         let args: Vec<String> = env::args().collect();
+        let mut initial_status = String::from("HELP: Ctrl-Q = quit");
         let document = if args.len() > 1 {
             let file_name = &args[1];
-            Document::open(file_name).unwrap_or_default()
+            // Document::open(file_name).unwrap_or_default();
+            let doc = Document::open(&file_name);
+            if doc.is_ok() {
+                doc.unwrap()
+            } else {
+                initial_status = format!("ERR: Could not open file: {file_name}");
+                Document::default()
+            }
         } else {
             Document::default()
         };
@@ -53,6 +77,7 @@ impl Editor {
             terminal: Terminal::default().expect("Failed to initialize terminal"),
             cursor_position: Position::default(),
             offset: Position::default(),
+            status_message: StatusMessage::from(initial_status),
             document,
         }
     }
@@ -109,6 +134,12 @@ impl Editor {
 
     fn draw_message_bar(&self) {
         Terminal::clear_current_line();
+        let message = &self.status_message;
+        if Instant::now() - message.time < Duration::new(5, 0) {
+            let mut text = message.text.clone();
+            text.truncate(self.terminal.size().width as usize);
+            print!("{text}");
+        }
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
