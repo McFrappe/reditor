82185d83065db044292c2994f51a2a4cdc7f7e2b
McFrappe <obeyfranso@gmail.com>
2023-05-30 11:31:17 +0200

    feat: can now scroll in document by incrementing cursor

--- src/document.rs
+++ src/document.rs
@@ -27,4 +27,8 @@ impl Document {
     #[must_use] pub fn is_empty(&self) -> bool {
         self.rows.is_empty()
     }
+
+    pub fn len(&self) -> usize {
+        self.rows.len()
+    }
 }
--- src/editor.rs
+++ src/editor.rs
@@ -6,8 +6,7 @@ use termion::event::Key;
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
 
-#[derive(Default)]
-pub struct Position {
+#[derive(Default)] pub struct Position {
     pub x: usize,
     pub y: usize,
 }
@@ -16,6 +15,7 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    offset: Position,
     document: Document,
 }
 
@@ -38,7 +38,7 @@ impl Editor {
         let args: Vec<String> = env::args().collect();
         let document = if args.len() > 1 {
             let file_name = &args[1];
-            Document::open(&file_name).unwrap_or_default()
+            Document::open(file_name).unwrap_or_default()
         } else {
             Document::default()
         };
@@ -47,6 +47,7 @@ impl Editor {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
             cursor_position: Position::default(),
+            offset: Position::default(),
             document,
         }
     }
@@ -85,13 +86,32 @@ impl Editor {
                 = pressed_key {
                     self.move_cursor(pressed_key);
         }
+        self.scroll();
         Ok(())
+    }
+
+    fn scroll(&mut self) {
+        let Position { x, y } =  self.cursor_position;
+        let width = self.terminal.size().width as usize;
+        let height = self.terminal.size().height as usize;
+        let mut offset = &mut self.offset;
+
+        if y < offset.y {
+            offset.y = y;
+        } else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+        if x < offset.x {
+            offset.x = x;
+        } else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
+        }
     }
 
     fn move_cursor(&mut self, key: Key) {
         let Position { mut y, mut x } = self.cursor_position;
         let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
+        let height = self.document.len();
         let width = size.width.saturating_sub(1) as usize;
 
         match key {
@@ -121,8 +141,9 @@ impl Editor {
     }
 
     pub fn draw_row(&self, row: &Row) {
-        let start = 0;
-        let end = self.terminal.size().width as usize;
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
         let row = row.render(start, end);
 
         println!("{row}\r");
@@ -132,7 +153,7 @@ impl Editor {
         let height = self.terminal.size().height;
         for terminal_row in 0..height - 1 {
             Terminal::clear_current_line();
-            if let Some(row) = self.document.row(terminal_row as usize) {
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
                 self.draw_row(row);
             } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
