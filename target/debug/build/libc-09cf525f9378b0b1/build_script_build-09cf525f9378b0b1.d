a39cbc89f232693bfbc69e1c4f00c7449e65976e
Fadde <obeyfranso@gmail.com>
2023-05-31 11:13:11 +0200

    Delete target directory

--- target/debug/build/libc-09cf525f9378b0b1/build_script_build-09cf525f9378b0b1.d
+++
@@ -1,5 +0,0 @@
-/Users/fadde/Projects/reditor/target/debug/build/libc-09cf525f9378b0b1/build_script_build-09cf525f9378b0b1: /Users/fadde/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.144/build.rs
-
-/Users/fadde/Projects/reditor/target/debug/build/libc-09cf525f9378b0b1/build_script_build-09cf525f9378b0b1.d: /Users/fadde/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.144/build.rs
-
-/Users/fadde/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.144/build.rs:
--- target/debug/deps/numtoa-fb99891ae73bce33.d
+++
@@ -1,7 +0,0 @@
-/Users/fadde/Projects/reditor/target/debug/deps/numtoa-fb99891ae73bce33.rmeta: /Users/fadde/.cargo/registry/src/github.com-1ecc6299db9ec823/numtoa-0.1.0/src/lib.rs
-
-/Users/fadde/Projects/reditor/target/debug/deps/libnumtoa-fb99891ae73bce33.rlib: /Users/fadde/.cargo/registry/src/github.com-1ecc6299db9ec823/numtoa-0.1.0/src/lib.rs
-
-/Users/fadde/Projects/reditor/target/debug/deps/numtoa-fb99891ae73bce33.d: /Users/fadde/.cargo/registry/src/github.com-1ecc6299db9ec823/numtoa-0.1.0/src/lib.rs
-
-/Users/fadde/.cargo/registry/src/github.com-1ecc6299db9ec823/numtoa-0.1.0/src/lib.rs:
